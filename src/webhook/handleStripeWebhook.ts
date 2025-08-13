8092a8f5d4532df36656bf57bc320e2c7f8f79f8
Rahad-Ullah <rahadullah10@gmail.com>
2025-08-13 10:16:25 +0600

    Fixed payment webhook issue

--- src/webhook/handleStripeWebhook.ts
+++ src/webhook/handleStripeWebhook.ts
@@ -6,33 +6,7 @@ import { Payment } from '../app/modules/payment/payment.model';
 import { randomUUID } from 'crypto';
 import { GiftCard } from '../app/modules/giftcard/gift-card.model';
 
-const handleStripeWebhook = async (req: Request, res: Response) => {
-      const signature = req.headers['stripe-signature'];
-      if (!signature) {
-            return res.status(400).json({ error: 'Missing stripe-signature header' });
-      }
-      try {
-            const event = stripe.webhooks.constructEvent(req.body, signature, config.stripe.webhook_secret as string);
 
-            switch (event.type) {
-                  case 'checkout.session.completed':
-                        await handlePaymentSuccess(event.data.object as Stripe.Checkout.Session);
-                        break;
-
-                  case 'payment_intent.payment_failed':
-
-                  default:
-                        console.log(`Unhandled event type: ${event.type}`);
-            }
-
-            return res.status(200).json({ received: true });
-      } catch (error) {
-            console.error('Webhook error:', error);
-            return res.status(400).json({ error: `Webhook error: ${(error as Error).message}` });
-      }
-};
-
-export default handleStripeWebhook;
 
 const handlePaymentSuccess = async (session: Stripe.Checkout.Session) => {
       const { metadata } = session;
@@ -78,4 +52,32 @@ const handlePaymentSuccess = async (session: Stripe.Checkout.Session) => {
                   }
             );
       }
+
+      return;
 };
+const handleStripeWebhook = async (req: Request, res: Response) => {
+      const signature = req.headers['stripe-signature'];
+      if (!signature) {
+            return res.status(400).json({ error: 'Missing stripe-signature header' });
+      }
+      try {
+            const event = stripe.webhooks.constructEvent(req.body, signature, config.stripe.webhook_secret as string);
+
+            switch (event.type) {
+                  case 'checkout.session.completed':
+                        await handlePaymentSuccess(event.data.object as Stripe.Checkout.Session);
+                        break;
+                  default:
+                        console.log(`Unhandled event type: ${event.type}`);
+            }
+
+            return res.status(200).json({ received: true });
+      } catch (error) {
+            console.error('Webhook error:', error);
+            return res.status(400).json({ error: `Webhook error: ${(error as Error).message}` });
+      }
+};
+
+export default handleStripeWebhook;
+
+
